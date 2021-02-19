fd11d5399442179e6f6c5c781ccb47702cdd37e4
alexfit4 <alex.ashtiany@gmail.com>
2021-02-18 21:16:57 -0500

    update api

--- models/manager.js
+++ models/manager.js
@@ -1,27 +1,28 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Manager extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Manager.init({
-    first_name: DataTypes.STRING,
-    last_name: DataTypes.STRING,
-    project_id: DataTypes.INTEGER,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Manager',
-  });
-  return Manager;
+	class Manager extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Manager.init(
+		{
+			first_name: DataTypes.STRING,
+			last_name: DataTypes.STRING,
+			project_id: DataTypes.INTEGER,
+			email: DataTypes.STRING,
+			password: DataTypes.STRING,
+		},
+		{
+			sequelize,
+			modelName: "Manager",
+		}
+	);
+	return Manager;
 };
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -52,3 +52,5 @@ module.exports = (app) => {
     ).then((dbManager) => res.json(dbManager));
   });
 };
+
+
