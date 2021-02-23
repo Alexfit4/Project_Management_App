2824cc3327055a530e57e6196563077eb2653b44
johnniesimpson <johnniesimpson@outlook.com>
2021-02-23 18:17:55 -0500

    Updated to latest server file.

--- server.js
+++ server.js
@@ -17,30 +17,10 @@ app.use(express.json());
 // Static directory
 app.use(express.static('public'));
 
-<<<<<<< HEAD
-// Import handlebars
-const exphbs = require('express-handlebars');
-
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-
-// Set the route
-const routes = require('./controllers/controller')
-
-app.use(routes);
-
-// Routes
-require('./routes/html-routes.js')(app);
-require('./routes/api-routes.js')(app);
-require('./routes/project-routes.js')(app);
-require('./routes/employee-routes.js')(app);
-=======
 app.engine('handlebars', expbs({
   defaultLayout: 'main',
   layoutsDir: path.join(__dirname, 'views/mainLayout' )
 }))
-
-
 
 // Routes
 require('./routes/html-routes')(app);
@@ -48,7 +28,6 @@ require('./routes/manager-api-routes.js')(app);
 require('./routes/project-api-routes.js')(app);
 require('./routes/employee-api-routes.js')(app);
 require('./routes/roles-api-routes.js')(app);
->>>>>>> 9fafe08a2febfad569f3ea08cc51c27da4bb0939
 
 // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync().then(() => {
