f022494b7324be94622b2635ee59841d94832744
alexfit4 <alex.ashtiany@gmail.com>
2021-02-23 21:21:28 -0500

    updated projects table

--- seeders/20210222195106-create-fake-projects.js
+++ seeders/20210222195106-create-fake-projects.js
@@ -1,46 +1,57 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+	up: async (queryInterface, Sequelize) => {
+		/**
+		 * Add seed commands here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkInsert('People', [{
+		 *   name: 'John Doe',
+		 *   isBetaMember: false
+		 * }], {});
+		 */
 
-    await queryInterface.bulkInsert('projects', [{
-      name: "TinCat",
-      createdAt: "2021-02-22 16:31:26",
-      updatedAt: '2021-02-22 16:31:26',        
-     },{
-      name: "Winery",
-      createdAt: '2021-02-22 16:31:47',
-      updatedAt: '2021-02-22 16:31:47', 
-     },
-     {
-      name: "Brewery",
-      createdAt: '2021-02-22 16:32:23',
-      updatedAt: '2021-02-22 16:32:23', 
-     },
-     {
-      name: "Amazon",
-      createdAt: '2021-02-22 16:32:40',
-      updatedAt: '2021-02-22 16:32:40', 
-     }], {});
-  },
+		await queryInterface.bulkInsert(
+			"projects",
+			[
+				{
+					name: "TinCat",
+					description: "Dating for cats!",
+					createdAt: "2021-02-22 16:31:26",
+					updatedAt: "2021-02-22 16:31:26",
+				},
+				{
+					name: "Winery",
+					description: "New Winery in town, needs a website",
+					createdAt: "2021-02-22 16:31:47",
+					updatedAt: "2021-02-22 16:31:47",
+				},
+				{
+					name: "Brewery",
+					description: "Brewery websites need revamping",
+					createdAt: "2021-02-22 16:32:23",
+					updatedAt: "2021-02-22 16:32:23",
+				},
+				{
+					name: "Amazon",
+					description: "Buy and sell items",
+					createdAt: "2021-02-22 16:32:40",
+					updatedAt: "2021-02-22 16:32:40",
+				},
+			],
+			{}
+		);
+	},
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+	down: async (queryInterface, Sequelize) => {
+		/**
+		 * Add commands to revert seed here.
+		 *
+		 * Example:
+		 * await queryInterface.bulkDelete('People', null, {});
+		 */
 
-    await queryInterface.bulkDelete('projects', null, {});
-  }
+		await queryInterface.bulkDelete("projects", null, {});
+	},
 };
