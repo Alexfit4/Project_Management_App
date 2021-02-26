5e8568140f599f0044f2635c3702c9ebb7140bfd
alexfit4 <alex.ashtiany@gmail.com>
2021-02-25 22:18:08 -0500

    changed user to upper case User

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 var bcrypt = require("bcryptjs");
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define("user", {
+  var User = sequelize.define("User", {
     // The email cannot be null, and must be a proper email before creation
     email: {
       type: DataTypes.STRING,
